dcfcc73e4da7e11a88ae7c06d71bcfb8aef56934
Seema Joshi <seema@Seemas-MacBook-Pro.local>
2018-10-19 13:58:02 -0400

    Added Readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # bamazon
+
+## Overview
+
+I created an Amazon-like storefront with the MySQL. The app takes in orders from customers and depletes stock from the store's inventory. 
+
+It uses the MySQL, esy-table and Inquirer npm packages. 
+
+1. I created a MySQL Database called `bamazon`.
+
+2. Then created a Table inside of that database called `products`.
+
+3. The products table has each of the following columns:
+
+   * item_id (unique id for each product)
+
+   * product_name (Name of product)
+
+   * department_name
+
+   * price (cost to customer)
+
+   * stock_quantity (how much of the product is available in stores)
+
+4. The database with 10 different products. 
+
+5. The create a Node application called `bamazonCustomer.js`. Running this application first display all of the items available for sale in a table format.
+
+6. The app then prompt users with two messages.
+
+   * The first should ask them the ID of the product they would like to buy.
+   * The second message should ask how many units of the product they would like to buy.
+
+7. Once the customer has placed the order, the application checks if the store has enough of the product to meet the customer's request.
+
+   * If not, the app displays `Insufficient quantity!`, and then prevent the order from going through.
+
+8. However, if the store _does_ have enough of the product, it fulfills the customer's order and updates the SQL database to reflect the remaining quantity and displays the total sale amount. 
+   
+## Overview
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,7 +31,7 @@ db.connect(err => {
 });
 
 const productsInv = () => {
-
+  // get all the data from the database and display 
   db.query("SELECT * FROM products", function (err, productsData) {
     if (err) throw err;
     console.log(Table.print(productsData));
@@ -45,24 +45,25 @@ const itemPrompt = (productsData) => {
   inquirer
     .prompt([{
       name: "itemToBuy",
-      message: "Please enter the item_id of the product you would like to purchase (Q to Quit).",
+      message: "Please enter the item_id of the product you would like to purchase.",
       type: "input",
       validate: function validate(name) {
-        return !isNaN(name) || name.toLowerCase === "q";
+        return !isNaN(name);
       }
     }]).then(userInput => {
 
-      // check if user input matches item_id
+      // check if user input matches any item_id in the database
       let flag = false;
       for (let i = 0; i < productsData.length; i++) {
         // console.log(productsData.length, userInput.itemToBuy);
         if (productsData[i].item_id === parseInt(userInput.itemToBuy)) {
+          // store the item_id, stock_quantity and price to a variable and pass as arguments to quantity function
           let productToBuy = productsData[i].item_id;
           let stock = productsData[i].stock_quantity;
           let unitCost = (productsData[i].price);
-          // let itemChosen = userInput.itemToBuy
+          
           flag = true;
-          console.log(productToBuy, stock, unitCost)
+          // console.log(productToBuy, stock, unitCost)
           quantityPrompt(productToBuy, stock, unitCost);
 
         }
@@ -71,42 +72,43 @@ const itemPrompt = (productsData) => {
 
         console.log("Incorrect item_id! Try again.");
         productsInv();
+        
       }
 
 
     });
 }
-
+// function to ask user for quantity of product to buy
 function quantityPrompt(productToBuy, stock, unitCost) {
-  console.log(productToBuy, stock, unitCost)
+  // console.log(productToBuy, stock, unitCost)
   inquirer
     .prompt([{
       name: "quantityToBuy",
-      message: "Please enter the quantity you would like to purchase (Q to Quit).",
+      message: "Please enter the quantity you would like to purchase.",
       type: "input",
-      validat: function validate(name) {
-        return parseInt(name) <= 0 || name.toLowerCase === "q";
+      validate: function(name) {
+        return !isNaN(name);
       }
     }]).then(quantity => {
-      
+      // check if user quantity is less then stock_quantity to allow purchase
       if (quantity.quantityToBuy > stock) {
         //   console.log(res.stock_quantity);
-        console.log(quantity.quantityToBuy);
+        // console.log(quantity.quantityToBuy);
         console.log("Not sufficient quantity for purchase! Try again!");
         productsInv();
 
       } else {
         let newStock = stock - quantity.quantityToBuy;
         let sales = quantity.quantityToBuy * unitCost;
-        console.log(quantity.quantityToBuy, unitCost);
-        console.log(productToBuy, newStock, sales);
+        // console.log(quantity.quantityToBuy, unitCost);
+        // console.log(productToBuy, newStock, sales);
         buyProduct(productToBuy, newStock, sales);
       }
     })
 };
 
 
-
+// function to update database - subtract user quantity from the stock_quantity
 function buyProduct(productToBuy, newStock, sales) {
   
   db.query("UPDATE products SET ? WHERE ?",
@@ -121,7 +123,7 @@ function buyProduct(productToBuy, newStock, sales) {
       
     function(error) {
       if (error) throw error;
-      console.log("Thank you for purchasing item id: " + productToBuy + " for total of $ " + sales + ".");
+      console.log("Thank you for purchasing item id: " + productToBuy + " for a total of $ " + sales + ".");
       productsInv();
     })
   };
